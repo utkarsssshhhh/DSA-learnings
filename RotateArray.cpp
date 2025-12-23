0124abb0a26882792f60f10e475345465769d705
Utkarsh <utkarshsharmays@gmail.com>
2025-12-23 12:50:32 +0530

    rotate array left by K places

--- RotateArray.cpp
+++ RotateArray.cpp
@@ -9,6 +9,20 @@ void transverse(int arr[], int n){
     cout << endl;
 }
 
+void reverse(int arr[], int st, int ed){
+    int n  = ed;
+    int i = st, j = ed-1;
+    while(i <= j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+
+    // transverse(arr, n);
+}
+
 
 void leftRotateByOnePlace(int arr[], int n){
     int temp = arr[0];
@@ -20,10 +34,39 @@ void leftRotateByOnePlace(int arr[], int n){
     transverse(arr, n);
 }
 
+void bruteForce(int arr[], int n, int k){
+    k = k % n;
+    int temp[k];
+    for(int i = 0; i<k; i++){
+        temp[i] = arr[i];
+    }
+    for(int i = k; i < n; i++){
+        arr[i-k] = arr[i];
+    }
+    int j = 0;
+    for(int i = n-k; i < n; i++){
+        arr[i] = temp[j];
+        j++;
+    }
+    transverse(arr, n);
+}
+
+void Optimal(int arr[], int n, int k){
+    k = k % n;
+    reverse(arr, 0, k);
+    reverse(arr, k, n);
+    reverse(arr, 0, n);
+
+    transverse(arr, n);
+}
+
 
 
 int main(){
     int n = 6;
     int arr[n] = {1, 2, 3, 4, 5, 6};
-    leftRotateByOnePlace(arr, n);
+    // leftRotateByOnePlace(arr, n);
+    // bruteForce(arr, 6, 2);
+    // reverse(arr, 0, n);
+    Optimal(arr, n, 2);
 }
